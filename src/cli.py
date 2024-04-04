e1d6cbc0b1995c506bae2144798d796930bcbe3f
Faiail <raffaele.scaringi20@gmail.com>
2024-04-04 15:08:29 +0200

    add split command to main

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,15 @@
 import click
+from src.utils import load_ruamel
 
 
 @click.group()
 def main():
     pass
 
+@main.command("split")
+@click.option(
+    "--parameters", help="Path to parameters file"
+)
+def split(parameters):
+    from src.preprocess import preprocess_normal
+    preprocess_normal(**load_ruamel(parameters))
