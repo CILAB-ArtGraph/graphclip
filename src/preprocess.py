18aa65f28142c9c2add05edde090a39f2e500eab
Faiail <raffaele.scaringi20@gmail.com>
2024-04-04 11:32:56 +0200

    add first preprocess script

---
+++ src/preprocess.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+
+
+def preprocess_normal(
+    dataset_path: str,
+    label_column: str,
+    val_size: float,
+    test_size: float,
+    outdir: str,
+    stratification: bool=True,
+    random_state: int = None,
+    **kwargs,
+):
+    dataset = pd.read_csv(dataset_path, **kwargs)
+    val_num = len(dataset) * val_size
+    test_num = len(dataset) * test_size
+    
+    
+    trainval, test = train_test_split(dataset, test_size=test_num, random_state=random_state, stratify=dataset[label_column] if stratification else None)
+    train, val = train_test_split(trainval, test_size=val_num, random_state=random_state, stratify=trainval[label_column] if stratification else None)
+    
+    # SAVE
+    os.makedirs(outdir)
+    train.to_csv(f"{outdir}/train.csv")
+    val.to_csv(f"{outdir}/val.csv")
+    test.to_csv(f"{outdir}/test.csv")
