f4207015b149dc90fa9cc2b96b48c05166badcee
Faiail <raffaele.scaringi20@gmail.com>
2024-04-04 15:08:07 +0200

    ensure int values and create out dir

--- src/preprocess.py
+++ src/preprocess.py
@@ -14,15 +14,15 @@ def preprocess_normal(
     **kwargs,
 ):
     dataset = pd.read_csv(dataset_path, **kwargs)
-    val_num = len(dataset) * val_size
-    test_num = len(dataset) * test_size
+    val_num = int(len(dataset) * val_size)
+    test_num = int(len(dataset) * test_size)
     
     
     trainval, test = train_test_split(dataset, test_size=test_num, random_state=random_state, stratify=dataset[label_column] if stratification else None)
     train, val = train_test_split(trainval, test_size=val_num, random_state=random_state, stratify=trainval[label_column] if stratification else None)
     
     # SAVE
-    os.makedirs(outdir)
+    os.makedirs(outdir, exist_ok=True)
     train.to_csv(f"{outdir}/train.csv")
     val.to_csv(f"{outdir}/val.csv")
     test.to_csv(f"{outdir}/test.csv")
