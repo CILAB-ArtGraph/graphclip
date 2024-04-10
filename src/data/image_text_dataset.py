f4414a4362e6c8a98dba36fb9e82637904a4f223
Faiail <raffaele.scaringi20@gmail.com>
2024-04-10 10:38:42 +0200

    add first version of new class

--- src/data/image_text_dataset.py
+++ src/data/image_text_dataset.py
@@ -8,6 +8,7 @@ import torch
 from torchvision.transforms import Compose
 import torchvision.transforms
 from open_clip import create_model_and_transforms
+import nlpaug.augmenter.word as naw
 
 
 class ImageTextDatset(Dataset):
@@ -28,9 +29,9 @@ class ImageTextDatset(Dataset):
             return None
         if isinstance(preprocess, Compose):
             return preprocess
-        if preprocess.get("name").lower() == 'clip':
+        if preprocess.get("name").lower() == "clip":
             return create_model_and_transforms(**preprocess.get("params"))[2]
-        
+
         assert preprocess.get("name").lower() == "compose"
         prep_types = torchvision.transforms.__dict__
         return Compose(
@@ -55,6 +56,35 @@ class ImageTextDatset(Dataset):
 
     def __len__(self):
         return len(self.dataset)
+
+
+class CLIPDataset(ImageTextDatset):
+    def __init__(
+        self,
+        dataset: Union[str, pd.DataFrame],
+        styles: Union[str, pd.DataFrame],
+        img_dir: str,
+        preprocess: Optional[Union[Compose, dict, str]] = None,
+        preprocess_classes: Optional[dict] = None,
+        **kwargs,
+    ):
+        super().__init__(dataset, img_dir, preprocess, **kwargs)
+        self.style = self._init_dataset(dataset=styles, **kwargs)
+
+    def _init_cls_preprocess(self, cls_preprocess: Optional[dict] = None) -> list:
+        if not cls_preprocess:
+            return None
+        augmentations = naw.__dict__
+        out = []
+        for entry, val in cls_preprocess.items():
+            out.append(augmentations[entry](**val).augment)
+        return out
+    
+    def augment_texts(self, texts: list[str]) -> list[str]:
+        pass
+
+    def collate_fn(self, batched_input: list[dict[str, Any]]) -> dict[str, Any]:
+        pass
 
 
 if __name__ == "__main__":
