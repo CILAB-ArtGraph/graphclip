bb8021d0759951784b55db55508f9e7a82a2445a
Faiail <raffaele.scaringi20@gmail.com>
2024-04-11 11:45:28 +0200

    add update bar and fix dataloader bug

--- src/experiment/run.py
+++ src/experiment/run.py
@@ -10,17 +10,21 @@ from tqdm import tqdm
 
 
 class Run:
-    def __init__(self, parameters):
+    def __init__(self, parameters: dict):
         self.parameters = parameters
 
     def get_bar(self, loader, desc):
-        if not self.parameters.get(ParameterKeys.BAR, False):
+        if not self.bar:
             return enumerate(loader)
         return tqdm(
             enumerate(loader),
             total=len(loader),
             desc=desc,
         )
+    def update_bar(self, bar, loss, **kwargs):
+        if not self.bar:
+            return
+        bar.set_postfix({ParameterKeys.LOSS: loss})
 
     def _init_model(self) -> Any:
         models = src.models.__dict__
@@ -45,7 +49,7 @@ class Run:
             return None
         if hasattr(dataset, "collate_fn"):
             loader_params_copy["collate_fn"] = dataset.collate_fn
-        return DataLoader(dataset, **loader_params)
+        return DataLoader(dataset, **loader_params_copy)
 
     def _init_dataloaders(self):
         dataset_params = deepcopy(self.parameters[ParameterKeys.DATASET])
@@ -53,13 +57,13 @@ class Run:
 
         train_params = dataset_params.get(ParameterKeys.TRAIN, None)
         train_data = self._init_dataset(train_params)
-
+        
         val_params = deepcopy(dataset_params.get(ParameterKeys.VAL, None))
         val_data = self._init_dataset(val_params)
 
         test_params = deepcopy(dataset_params.get(ParameterKeys.TEST, None))
         test_data = self._init_dataset(test_params)
-
+        
         return (
             self._init_loader(train_data, loader_params),
             self._init_loader(val_data, loader_params),
@@ -68,7 +72,7 @@ class Run:
 
     def _init_metrics(self):
         metrics_types = torchmetrics.__dict__
-        metrics_params = deepcopy(self.parameters.get(ParameterKeys.METRICS))
+        metrics_params = deepcopy(self.parameters.get(ParameterKeys.METRICS, {}))
         return {k: metrics_types[v.get(ParameterKeys.NAME)](**v.get(ParameterKeys.PARAMS)) for k, v in metrics_params.items()}
 
     def launch(self):
