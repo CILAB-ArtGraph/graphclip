5f69769ad5c4bab97da134d05d2d9af455f08b9a
Faiail <raffaele.scaringi20@gmail.com>
2024-04-11 11:44:14 +0200

    point to corrent entries

--- src/loss/clip_image_text_loss.py
+++ src/loss/clip_image_text_loss.py
@@ -1,17 +1,24 @@
 import torch
 from torch.nn.functional import cross_entropy
 
+
 class CLIPImageTextLoss(torch.nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        
+    def __init__(self, return_logits: bool = False) -> None:
+        super().__init__()
+        self.return_logits=return_logits
+
     def get_logits(self, pred: dict[str, torch.Tensor]) -> torch.Tensor:
         # gets image scaled logits
-        image_embeddings = pred.get("image")
-        text_embeddings = pred.get("text")
-        scale = pred.get("scale")
-        return scale * image_embeddings @ text_embeddings.T 
-    
-    def forward(self, pred: dict[str, torch.Tensor], target: torch.Tensor) -> torch.Tensor:
+        image_embeddings = pred.get("image_features")
+        text_embeddings = pred.get("text_features")
+        scale = pred.get("logit_scale")
+        return scale * image_embeddings @ text_embeddings.T
+
+    def forward(
+        self, pred: dict[str, torch.Tensor], target: torch.Tensor
+    ) -> torch.Tensor:
         image_logits = self.get_logits(pred=pred)
-        return cross_entropy(input=image_logits, target=target)
+        out =  {"loss": cross_entropy(input=image_logits, target=target)}
+        if self.return_logits:
+            out["logits"] = image_logits
+        return out 
