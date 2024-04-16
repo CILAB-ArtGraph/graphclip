872bb8ef005e98f0aa945dc7ec2e2fa66df95d02
Faiail <raffaele.scaringi20@gmail.com>
2024-04-16 18:30:28 +0200

    extract non-artwork nodes

--- src/features/extract_label_feats.py
+++ src/features/extract_label_feats.py
@@ -9,19 +9,17 @@ from safetensors.torch import save_file
 
 
 class LabelDataset(Dataset):
-    def __init__(self, data_path, tokenizer):
-        self._data = pd.read_csv(data_path, header=None)
-        print(self._data)
-        input()
-        self._tokenizer = tokenizer
-        print(f"Having {len(self._data)} instances!")
+    def __init__(self, data, tokenizer, target_col=1, **in_kwargs):
+        self.data = pd.read_csv(data, **in_kwargs) if isinstance(data, str) else data
+        self.tokenizer = tokenizer
+        self.target_col = target_col
 
     def __getitem__(self, item):
-        name = self._data.iloc[item, 1]
-        return name, self._tokenizer(name).squeeze()
+        name = self.data.iloc[item, self.target_col]
+        return self.tokenizer(name).squeeze()
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
 
 def get_labels(in_dir, exceptions, mapping):
@@ -35,25 +33,33 @@ def get_labels(in_dir, exceptions, mapping):
 
 
 @torch.no_grad()
-def main():
-    parameters = load_ruamel("./configs/features/extract_label_txt_feats.yaml")
-    device = parameters["device"]
-    model, _, _ = open_clip.create_model_and_transforms(**parameters["model"])
+def extract_label_clip(parameters: dict):
+    device = parameters.get("device")
+    model, _, _ = open_clip.create_model_and_transforms(**parameters.get("model"))
     model = model.to(device)
     tokenizer = open_clip.get_tokenizer(parameters["tokenizer"])
-    labels = get_labels(**parameters["labels"])
-    out_dir = parameters["out_dir"]
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir, exist_ok=True)
+    labels = get_labels(**parameters.get("labels"))
+    external_source = parameters.get("source", {})
+    out_dir = parameters.get("out_dir")
+    os.makedirs(out_dir, exist_ok=True)
+    root_dir = parameters.get("labels").get("in_dir")
     for lab, filename in labels:
-        print(f"Doing {lab}")
+        print(f"Extracting textual features for {lab}...")
+        target_df = pd.read_csv(f'{root_dir}/{filename}', header=None)
+        lab_source_fname = external_source.get(lab, None)
+        target_col=1
+        if lab_source_fname is not None:
+            lab_source = pd.read_csv(lab_source_fname)
+            target_df = pd.merge(left=target_df, right=lab_source, left_on=1, right_on="style")
+            target_col=2
         dataset = LabelDataset(
-            data_path=f'{parameters["labels"]["in_dir"]}/{filename}',
+            data=target_df,
             tokenizer=tokenizer,
+            target_col=target_col,
         )
         loader = DataLoader(dataset, **parameters["dataset"])
         tensors = None
-        for names, tokens in tqdm(loader):
+        for tokens in tqdm(loader):
             tokens = tokens.to(device)
             embeddings = model.encode_text(tokens)
             tensors = (
@@ -61,7 +67,3 @@ def main():
             )
         save_file({"embeddings": tensors}, f"{out_dir}/{lab}.safetensors")
         print("Done!")
-
-
-if __name__ == "__main__":
-    main()
