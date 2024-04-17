bb5339a4e2205e9c010f7081d6e37dd4480545b1
Faiail <raffaele.scaringi20@gmail.com>
2024-04-17 10:45:10 +0200

    add load tensor

--- src/utils/external.py
+++ src/utils/external.py
@@ -1,7 +1,13 @@
 from ruamel.yaml import YAML
 from pathlib import Path
+from safetensors import safe_open
 
 
 def load_ruamel(path:str, typ:str="safe") -> dict:
     yaml = YAML(typ=typ)
     return yaml.load(Path(path))
+
+def load_tensor(file, key, device='cpu', framework='pt'):
+    with safe_open(file, device=device, framework=framework) as f:
+        tensor = f.get_tensor(key)
+    return tensor
