c8d3d53d0dee66203ef9fc57025871891601f4b1
Faiail <raffaele.scaringi20@gmail.com>
2024-04-23 10:53:22 +0200

    unique bash file

---
+++ launch_gat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbatch  -o gat3experiment.out -e gat3experiment.out ./slurms/proposed_model/normal_clip_graph_gat/launch --parameters="./configs_cineca/proposed_model/gat/normal_clip_graph_gat3.yaml" --graph
+sbatch  -o gat2experiment.out -e gat2experiment.out ./slurms/proposed_model/normal_clip_graph_gat/launch --parameters="./configs_cineca/proposed_model/gat/normal_clip_graph_gat2.yaml" --graph
