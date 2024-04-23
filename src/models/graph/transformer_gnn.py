fa5e5e6c6c4fb7b11826dbf00cc2386cb3dd10e4
Faiail <raffaele.scaringi20@gmail.com>
2024-04-23 17:18:08 +0200

    add static variables

--- src/models/graph/transformer_gnn.py
+++ src/models/graph/transformer_gnn.py
@@ -4,5 +4,10 @@ from torch_geometric.nn.models.basic_gnn import BasicGNN
 
 
 class TransformerGNN(BasicGNN):
-    def init_conv(self, in_channels: int | Tuple[int], out_channels: int, **kwargs) -> MessagePassing:
+    supports_edge_weight = False
+    supports_edge_attr = True
+
+    def init_conv(
+        self, in_channels: int | Tuple[int], out_channels: int, **kwargs
+    ) -> MessagePassing:
         return TransformerConv(in_channels, out_channels, **kwargs)
