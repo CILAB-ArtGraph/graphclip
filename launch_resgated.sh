3070c50299a65775ac68093874f3dc56617b62a6
Faiail <raffaele.scaringi20@gmail.com>
2024-04-23 16:40:35 +0200

    naming all experiments in sbatch list

--- launch_resgated.sh
+++ launch_resgated.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sbatch  -o res3_sigmoid_experiment.out -e res3_sigmoid_experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3.yaml" --graph
-sbatch  -o res3_tanh_experiment.out -e res3_tanh_experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3_tanh.yaml" --graph
-sbatch  -o res2_sigmoid_experiment.out -e res2_sigmoid_experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated2.yaml" --graph
-sbatch  -o res2_tanh_experiment.out -e res2_tanh_experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3_tanh.yaml" --graph
+sbatch  -o res3_sigmoid_experiment.out -e res3_sigmoid_experiment.out --job-name ressig3exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3.yaml" --graph
+sbatch  -o res3_tanh_experiment.out -e res3_tanh_experiment.out --job-name restan3exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3_tanh.yaml" --graph
+sbatch  -o res2_sigmoid_experiment.out -e res2_sigmoid_experiment.out --job-name ressig2exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated2.yaml" --graph
+sbatch  -o res2_tanh_experiment.out -e res2_tanh_experiment.out --job-name restan2exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/resgated/normal_clip_graph_resgated3_tanh.yaml" --graph
--- launch_transformer.sh
+++ launch_transformer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbatch  -o transformer3experiment.out -e transformer3experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/transformer/normal_clip_graph_transformer3.yaml" --graph
-sbatch  -o transformer2experiment.out -e transformer2experiment.out ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/transformer/normal_clip_graph_transformer2.yaml" --graph
+sbatch  -o transformer3experiment.out -e transformer3experiment.out --job-name trans3exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/transformer/normal_clip_graph_transformer3.yaml" --graph
+sbatch  -o transformer2experiment.out -e transformer2experiment.out --job-name trans2exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/transformer/normal_clip_graph_transformer2.yaml" --graph
