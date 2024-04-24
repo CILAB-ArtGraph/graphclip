dd6fa2047eb7fc4eb363041de7fc1e6932b14c1e
Faiail <raffaele.scaringi20@gmail.com>
2024-04-24 15:59:05 +0200

    remove useless bash files

--- launch_gcn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sbatch  -o gcn3experiment.out -e gcn3experiment.out --job-name gcn3exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/gcn/normal_clip_graph_gcn3.yaml" --graph
-sbatch  -o gcn2experiment.out -e gcn2experiment.out --job-name gcn2exp ./slurms/proposed_model/launch --parameters="./configs_cineca/proposed_model/gcn/normal_clip_graph_gcn2.yaml" --graph
