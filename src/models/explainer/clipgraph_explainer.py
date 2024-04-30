113bf743d141a3a9a0928cffa3347c7732c9e2fe
Faiail <raffaele.scaringi20@gmail.com>
2024-04-30 17:31:02 +0200

    add complete graph explanation

--- src/models/explainer/clipgraph_explainer.py
+++ src/models/explainer/clipgraph_explainer.py
@@ -2,13 +2,15 @@ from torch import Tensor
 from torch_geometric.data import Data, HeteroData
 from .clip_explainer import VisualWrapperForExplanation
 from .abstract_explainer import AbstractExplainer
-from typing import Any, Union
+from typing import Any, Union, Tuple, List
 from torchvision.transforms import Compose
 from open_clip import create_model_and_transforms
 from src.models import CLIPGraph
-from torch_geometric.explain import ExplainerAlgorithm, Explainer
+from torch_geometric.explain import ExplainerAlgorithm, Explainer, HeteroExplanation
 from .gradcam import apply_gradcam, visualize_gradcam, GradCAM
 import torch
+from copy import deepcopy
+from yfiles_jupyter_graphs import GraphWidget
 
 
 class GraphWrapperForExplanation(torch.nn.Module):
@@ -38,7 +40,9 @@ class CLIPGraphExplainer(AbstractExplainer):
     ) -> None:
         super().__init__()
         self.device = device
-        self.image_preprocess = self._get_image_preprocess(image_preprocess=image_preprocess)
+        self.image_preprocess = self._get_image_preprocess(
+            image_preprocess=image_preprocess
+        )
 
     def _get_image_preprocess(self, image_preprocess: Union[str, Compose]) -> Compose:
         if isinstance(image_preprocess, Compose):
@@ -74,6 +78,95 @@ class CLIPGraphExplainer(AbstractExplainer):
             else visualize_gradcam(image_path=img_path, gweights=cam)
         )
 
+    def plot_explanation(
+        self,
+        explanation: HeteroExplanation,
+        metadata: Tuple[List[str]],
+        graph: HeteroData,
+    ):
+        subgraph_nodes, subgraph_edges = self.get_exp_subgraph(
+            explanation=explanation, metadata=metadata
+        )
+        w = GraphWidget()
+
+        # nodes
+        w.nodes = [
+            {"id": f"{t}_{idx}", "properties": {"label": t}}
+            for t in subgraph_nodes.keys()
+            for idx in subgraph_nodes[t]
+        ]
+
+        colors = [
+            # yellow
+            (255, 255, 0),
+            # red
+            (255, 0, 0),
+            # green
+            (0, 255, 0),
+            # blue
+            (0, 0, 255),
+            # purple
+            (255, 0, 255),
+            # cyan
+            (0, 255, 255),
+            # orange
+            (255, 165, 0),
+            # pink
+            (255, 192, 203),
+            # brown
+            (139, 69, 19),
+            # grey
+            (128, 128, 128),
+            # black
+            (0, 0, 0),
+        ] * 3
+
+        color_map = {node: color for node, color in zip(metadata[0], colors)}
+        w.set_node_color_mapping(lambda x: color_map[x["properties"]["label"]])
+
+        w.edges = [
+            {"start": f"{h_t}_{h_idx}", "end": f"{t_t}_{t_idx}"}
+            for (h_t, _, t_t) in subgraph_edges.keys()
+            for (h_idx, t_idx) in subgraph_edges[(h_t, _, t_t)]
+        ]
+
+        return w
+
+    def get_exp_subgraph(
+        self,
+        explanation: HeteroExplanation,
+        metadata: Tuple[List[str]],
+    ):
+        exp_mod_nodes = {node_t: None for node_t in metadata[0]}
+        exp_mod_edges = {edge_t: None for edge_t in metadata[1]}
+
+        # nodes
+        for node_t in metadata[0]:
+            exp_mod_nodes[node_t] = list(
+                map(
+                    lambda x: x[0],
+                    filter(
+                        lambda x: x[1] > 0,
+                        enumerate(explanation[node_t].node_mask.mean(dim=1).tolist()),
+                    ),
+                )
+            )
+
+        # edges
+        for edge_t in metadata[1]:
+            edges = list(
+                map(
+                    lambda x: x[0],
+                    filter(
+                        lambda x: x[1] > 0,
+                        enumerate(explanation[edge_t].edge_mask.tolist()),
+                    ),
+                )
+            )
+            exp_mod_edges[edge_t] = explanation[edge_t].edge_index[:, edges].T.tolist()
+
+        return exp_mod_nodes, exp_mod_edges
+
     def explain_graph(
         self,
         model: CLIPGraph,
@@ -90,4 +183,7 @@ class CLIPGraphExplainer(AbstractExplainer):
             target_node=target_node,
         ).to(self.device)
         explainer = Explainer(model=graph_model_wrap, algorithm=algo, **algo_kwargs)
-        return explainer(graph.x_dict, edge_index=graph.edge_index_dict)
+        explanation = explainer(graph.x_dict, edge_index=graph.edge_index_dict)
+        return (
+            explanation if not plot else self.plot_explanation(explanation=explanation)
+        )
