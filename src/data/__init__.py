3b3ed9e64b9721426c0f18e939a7e1083c846006
Faiail <raffaele.scaringi20@gmail.com>
2024-05-03 11:50:31 +0200

    add top level import

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,4 @@
 from .image_text_dataset import ImageTextDatset, CLIPDataset
 from .utils import DataDict
 from .image_graph_dataset import ImageGraphDataset
+from .image_dataset import ImageDataset
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from open_clip import CLIP
 from .clip_graph import CLIPGraph
+from .vit import ViT
