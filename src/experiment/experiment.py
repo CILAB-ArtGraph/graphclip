ce7d673a76a4cf9027411be01b8457fa9689d7b6
Faiail <raffaele.scaringi20@gmail.com>
2024-05-03 13:10:19 +0200

    simpler experiment interface

--- src/experiment/experiment.py
+++ src/experiment/experiment.py
@@ -1,6 +1,7 @@
 from .clip_run import CLIPRun
 from .clip_graph_run import CLIPGraphRun
 from .vit_run import ViTRun
+from .run import Run
 import json
 from .utils import ParameterKeys
 import os
@@ -17,17 +18,8 @@ def test_clip(parameters):
         json.dump(test_metrics, f)
 
 
-def fine_tune_clip(parameters):
-    run = CLIPRun(parameters=parameters)
-    test_metrics = run.launch()
-    print(test_metrics)
-    out_dir = parameters.get(ParameterKeys.OUT_DIR)
-    with open(f"{out_dir}/test_metrics.json", "w+") as f:
-        json.dump(test_metrics, f)
-
-
-def fine_tune_vit(parameters):
-    run = ViTRun(parameters=parameters)
+def launch_experiment(parameters, run_cls: Run):
+    run = run_cls(parameters)
     test_metrics = run.launch()
     print(test_metrics)
     out_dir = parameters.get(ParameterKeys.OUT_DIR)
@@ -37,17 +29,9 @@ def fine_tune_vit(parameters):
 
 def run_experiment(parameters, graph: bool = False, ablation: bool = False):
     if ablation:
-        fine_tune_vit(parameters)
+        run_cls = ViTRun
     elif not graph:
-        fine_tune_clip(parameters=parameters)
+        run_cls = CLIPGraphRun
     else:
-        train_clip_graph(parameters=parameters)
-
-
-def train_clip_graph(parameters):
-    run = CLIPGraphRun(parameters=parameters)
-    test_metrics = run.launch()
-    print(test_metrics)
-    out_dir = parameters.get(ParameterKeys.OUT_DIR)
-    with open(f"{out_dir}/test_metrics.json", "w+") as f:
-        json.dump(test_metrics, f)
+        run_cls = CLIPRun
+    launch_experiment(parameters=parameters, run_cls=run_cls)
