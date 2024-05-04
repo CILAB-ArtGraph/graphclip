d0e2cafbdec7cfe33e8ada9ee9e960fbc30f5505
Faiail <raffaele.scaringi20@gmail.com>
2024-05-04 12:13:54 +0200

    fix typo

--- launch_fine_tuning_style.sh
+++ launch_fine_tuning_style.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sbatch -o outs/baseline_style_naive_ft.out -e outs/baseline_style_naive_ft.out ./slurms/baselines/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_naive.yaml"
-sbatch -o outs/baseline_style_wiki_ft.out -e outs/baseline_style_wiki_ft.out ./slurms/baselines/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_wikipedia.yaml"
-sbatch -o outs/baseline_style_mistral_ft.out -e outs/baseline_style_mistral_ft.out ./slurms/baselines/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_mistral.yaml"
-sbatch -o outs/baseline_style_prot_ft.out -e outs/baseline_style_prot_ft.out ./slurms/baselines/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_mistral_prototype.yaml"
+sbatch -o outs/baseline_style_naive_ft.out -e outs/baseline_style_naive_ft.out ./slurms/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_naive.yaml"
+sbatch -o outs/baseline_style_wiki_ft.out -e outs/baseline_style_wiki_ft.out ./slurms/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_wikipedia.yaml"
+sbatch -o outs/baseline_style_mistral_ft.out -e outs/baseline_style_mistral_ft.out ./slurms/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_mistral.yaml"
+sbatch -o outs/baseline_style_prot_ft.out -e outs/baseline_style_prot_ft.out ./slurms/baselines/fine_tuning/launch --parameters="configs_cineca/baselines/style/normal_clip_fine_tuning_mistral_prototype.yaml"
