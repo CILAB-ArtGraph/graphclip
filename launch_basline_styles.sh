ac9678ed98327755fd43099c86156d1945ec109e
Faiail <raffaele.scaringi20@gmail.com>
2024-05-04 12:11:52 +0200

    adds fine tuning scripts

---
+++ launch_basline_styles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./launch_no_training_style.sh
+./launch_fine_tuning_style.sh
---
+++ launch_no_training_style.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sbatch -o outs/baseline_style_naive.out -e outs/baseline_style_naive.out ./slurms/baselines/baselines/no_training/launch --parameters="configs_cineca/baselines/style/normal_clip_no_training.yaml"
+sbatch -o outs/baseline_style_wiki.out -e outs/baseline_style_wiki.out ./slurms/baselines/baselines/no_training/launch --parameters="configs_cineca/baselines/style/normal_clip_no_training_wikipedia.yaml"
+sbatch -o outs/baseline_style_mistral.out -e outs/baseline_style_mistral.out ./slurms/baselines/baselines/no_training/launch --parameters="configs_cineca/baselines/style/normal_clip_no_training_mistral.yaml"
+sbatch -o outs/baseline_style_prot.out -e outs/baseline_style_prot.out ./slurms/baselines/baselines/no_training/launch --parameters="configs_cineca/baselines/style/normal_clip_no_training_mistral_prototype.yaml"
