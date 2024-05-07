93035717faca123bfe50c283deabc454e9dee153
Faiail <raffaele.scaringi20@gmail.com>
2024-05-07 14:56:21 +0200

    top level imports (useful for run)

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,4 +2,4 @@ from .image_text_dataset import ImageTextDatset, CLIPDataset
 from .utils import DataDict
 from .image_graph_dataset import ImageGraphDataset
 from .image_dataset import ImageDataset
-from .image_text_multiclass_dataset import CLIPMultiTaskDataset
+from .image_text_multiclass_dataset import CLIPMultiTaskDataset, ImageTextMultiTaskDataset
