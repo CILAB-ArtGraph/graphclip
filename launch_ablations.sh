30fe2db3cad6f2cc3404362465e449fb04e440c8
Faiail <raffaele.scaringi20@gmail.com>
2024-05-07 17:40:19 +0200

    add multitask run to script

--- launch_ablations.sh
+++ launch_ablations.sh
@@ -3,3 +3,4 @@
 mkdir outs/ablations
 sbatch -o ./outs/ablations/vit_style.out -e ./outs/ablations/vit_style.out --job-name vit_style ./slurms/baselines/fine_tuning/launch --parameters="./configs_cineca/baselines/style/vit.yaml" --ablation
 sbatch -o ./outs/ablations/vit_genre.out -e ./outs/ablations/vit_genre.out --job-name vit_genre ./slurms/baselines/fine_tuning/launch --parameters="./configs_cineca/baselines/genre/vit.yaml" --ablation
+sbatch -o ./outs/ablations/vit_multitask.out -e ./outs/ablations/vit_multitask.out --job-name vit_multi ./slurms/baselines/fine_tuning/launch --parameters="./configs_cineca/baselines/multitask/vit.yaml" --ablation --multitask
