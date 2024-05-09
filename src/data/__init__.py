8da4d5d46c46568ae340180b06da2c528177a02e
Faiail <raffaele.scaringi20@gmail.com>
2024-05-09 15:45:57 +0200

    top level import

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,5 +2,9 @@ from .image_text_dataset import ImageTextDatset, CLIPDataset
 from .utils import DataDict
 from .image_graph_dataset import ImageGraphDataset
 from .image_dataset import ImageDataset
-from .image_text_multitask_dataset import CLIPMultiTaskDataset, ImageTextMultiTaskDataset
+from .image_text_multitask_dataset import (
+    CLIPMultiTaskDataset,
+    ImageTextMultiTaskDataset,
+)
 from .image_multitask_dataset import ImageMultiTaskDataset
+from .image_graph_multitask_dataset import ImageGraphMultiTaskDataset
