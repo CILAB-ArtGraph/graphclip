80aa55540f72cb3e4fd57b68b75a547041241cd3
Faiail <raffaele.scaringi20@gmail.com>
2024-05-09 15:06:40 +0200

    rename

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,5 +2,5 @@ from .image_text_dataset import ImageTextDatset, CLIPDataset
 from .utils import DataDict
 from .image_graph_dataset import ImageGraphDataset
 from .image_dataset import ImageDataset
-from .image_text_multiclass_dataset import CLIPMultiTaskDataset, ImageTextMultiTaskDataset
+from .image_text_multitask_dataset import CLIPMultiTaskDataset, ImageTextMultiTaskDataset
 from .image_multitask_dataset import ImageMultiTaskDataset
