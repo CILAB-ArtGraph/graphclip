a5f482cd7bdc5ca8dd3a0c4e358f53520e7248f3
Faiail <raffaele.scaringi20@gmail.com>
2024-05-09 16:42:59 +0200

    handle multi task prediction

--- src/loss/clip_graph_loss.py
+++ src/loss/clip_graph_loss.py
@@ -2,12 +2,15 @@ from torch._tensor import Tensor
 from .clip_image_text_loss import CLIPImageTextLoss
 
 class CLIPImageGraphLoss(CLIPImageTextLoss):
-    def __init__(self, return_logits: bool = False) -> None:
+    def __init__(self, return_logits: bool = False, target_node: str = None) -> None:
         super().__init__(return_logits)
+        self.target_node = target_node
     
     def get_logits(self, pred: dict[str, Tensor]) -> Tensor:
         # gets image scaled logits
         image_embeddings = pred.get("image_features")
         graph_embeddings = pred.get("graph_features")
+        if self.target_node:
+            graph_embeddings = graph_embeddings[self.target_node]
         scale = pred.get("logit_scale")
         return scale * image_embeddings @ graph_embeddings.T
